a39664cb9ffbfdf083b57392e654bb035c0f4b7f
Timo <timolingen@gmail.com>
2025-07-14 22:43:10 +0200

    --- change signal file extension---

--- dropbox_writer.py
+++ dropbox_writer.py
@@ -9,7 +9,7 @@ DROPBOX_ACCESS_TOKEN = os.getenv("DROPBOX_ACCESS_TOKEN")
 def upload_signal_to_dropbox(signal_data: dict, filename_prefix="signal"):
     dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
     timestamp = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
-    filename = f"/{filename_prefix}_{timestamp}.json"
+    filename = f"/{filename_prefix}_{timestamp}.txt"
     content = signal_data.__str__()
 
     dbx.files_upload(
