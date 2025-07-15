d0cc9f7f3bfaea16a5697c749de0184746c8e8ec
Timo <timolingen@gmail.com>
2025-07-15 10:18:26 +0200

    --- dropbox oauth integration---

--- dropbox_writer.py
+++ dropbox_writer.py
@@ -4,10 +4,18 @@ import os
 import json
 from datetime import datetime
 
-DROPBOX_ACCESS_TOKEN = os.getenv("DROPBOX_ACCESS_TOKEN")
+#DROPBOX_ACCESS_TOKEN = os.getenv("DROPBOX_ACCESS_TOKEN")
+DROPBOX_REFRESH_TOKEN = os.getenv("DROPBOX_REFRESH_TOKEN")
+DROPBOX_APP_KEY = os.getenv("DROPBOX_APP_KEY")
+DROPBOX_APP_SECRET = os.getenv("DROPBOX_APP_SECRET")
 
 def upload_signal_to_dropbox(signal_data: dict, filename_prefix="signal"):
-    dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
+    dbx = dropbox.Dropbox(
+        #oauth2_access_token=DROPBOX_ACCESS_TOKEN,
+        oauth2_refresh_token=DROPBOX_REFRESH_TOKEN,
+        app_key=DROPBOX_APP_KEY,
+        app_secret=DROPBOX_APP_SECRET
+    )
     timestamp = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
     filename = f"/{filename_prefix}_{timestamp}.txt"
     content = signal_data.__str__()
